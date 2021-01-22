3bb23c7ce3ee7ed444ab59a54eac3adee7956987
Tezla-Lee <tezla.lee@gmail.com>
2021-01-22 10:37:12 +0900

    feat: Add LocationDto

--- src/main/java/com/h_j/map/model/Location.java
+++ src/main/java/com/h_j/map/model/Location.java
@@ -1,11 +1,17 @@
 package com.h_j.map.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Location {
     @Id
     @NonNull
