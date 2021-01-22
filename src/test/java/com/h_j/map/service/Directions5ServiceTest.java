3ff400d79dd003c8df68d34f65f2d1b875bd57b2
Tezla-Lee <tezla.lee@gmail.com>
2021-01-22 12:45:35 +0900

    test: Add Directions 5 Service test

--- src/test/java/com/h_j/map/service/Directions5ServiceTest.java
+++ src/test/java/com/h_j/map/service/Directions5ServiceTest.java
@@ -1,26 +1,29 @@
 package com.h_j.map.service;
 
 import com.h_j.map.dto.LocationDto;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@WebMvcTest(controllers = Directions5Service.class)
-@RequiredArgsConstructor
+@ExtendWith(MockitoExtension.class)
 class Directions5ServiceTest {
 
-    @MockBean
+    @Mock
     private Directions5Service directions5Service;
 
     @Test
     public void getDirections5() {
+        // 출발
         LocationDto departure = new LocationDto();
         departure.setLongitude("127.1058342");
         departure.setLatitude("37.359708");
+
+        // 도착
         LocationDto destination = new LocationDto();
         destination.setLongitude("129.075986");
         destination.setLatitude("35.179470");
+
         directions5Service.getDirections5(departure, destination);
     }
 }
