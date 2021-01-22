70836ecaa1ae5fa4a2b780e82dc58b734dfcfd6b
Tezla-Lee <tezla.lee@gmail.com>
2021-01-22 12:04:13 +0900

    feat: Add Directions 5 Dto

---
+++ src/main/java/com/h_j/map/dto/Directions5Dto.java
@@ -0,0 +1,11 @@
+package com.h_j.map.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Directions5Dto {
+    String start;
+    String goal;
+}
