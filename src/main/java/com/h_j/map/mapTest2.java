f546103c048ca6e1996a5d974129d7835234b63a
Hansol-Jeong <shn03014@naver.com>
2021-01-22 10:02:58 +0900

    feat: add test

--- src/main/java/com/h_j/map/mapTest2.java
+++
@@ -1,4 +0,0 @@
-package com.h_j.map;
-
-public class mapTest2 {
-}
--- src/main/java/com/h_j/map/maptest.java
+++ src/main/java/com/h_j/map/maptest.java
@@ -1,10 +1,15 @@
 package com.h_j.map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+
 public class maptest {
 
     public static void main(String[] args) {
@@ -12,28 +17,48 @@ public class maptest {
         String clientSecret = "UOEzYHQGVrBh7PDty5kWKTrELIebbFwyWTEYYLRP";  //clientSecret
 
         try {
-            String addr = URLEncoder.encode("그린팩토리", "UTF-8");  //주소입력
-            String apiURL = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=분당구 불정로 6"; //json
-            //String apiURL = "https://openapi.naver.com/v1/map/geocode.xml?query=" + addr; // xml
-            URL url = new URL(apiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
-            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if(responseCode==200) {
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+           String addr = URLEncoder.encode("여수","utf-8");
+           String api = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=" + addr;
+           StringBuffer sb = new StringBuffer();
+           URL url = new URL(api);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestProperty("Content-Type", "application/json");
+            http.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            http.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            http.setRequestMethod("GET");
+            http.connect();
+
+            InputStreamReader in = new InputStreamReader(http.getInputStream(),"utf-8");
+            BufferedReader br = new BufferedReader(in);
+            String line;
+            while((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
             }
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
+
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject;
+            JSONObject jsonObject2;
+            JSONArray jsonArray;
+            String x = "";
+            String y = "";
+
+            jsonObject = (JSONObject) parser.parse(sb.toString());
+            jsonArray = (JSONArray)jsonObject.get("addresses");
+            for(int i = 0; i<jsonArray.size(); i++) {
+                jsonObject2 = (JSONObject) jsonArray.get(i);
+                if(null != jsonObject2.get("x")) {
+                    x = jsonObject2.get("x").toString();
+                }
+                if(null != jsonObject2.get("y")) {
+                    y = jsonObject2.get("y").toString();
+                }
             }
             br.close();
-            System.out.println(response.toString());
+            in.close();
+
+            http.disconnect();
+            System.out.println("위도 : " + y + "경도 : " + x);
+
         } catch (Exception e) {
             System.out.println(e);
         }
